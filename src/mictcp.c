4f66e9b0ea1d8244f7919b5450af53de5e99d297
RomainMln <moulin.romain@hotmail.com>
2022-04-11 08:50:31 +0200

    v4.1

--- src/mictcp.c
+++ src/mictcp.c
@@ -1,14 +1,18 @@
 #include <mictcp.h>
 #include <api/mictcp_core.h>
+#include <pthread.h>
 
 mic_tcp_sock sock_source;
 mic_tcp_sock sock_puit;
 mic_tcp_sock_addr addr_dest;
 
-mic_tcp_pdu pdu;
 short PE = 0;
 short PA =0;
 short msg_passe =0;
+short msg_conseq = 4;
+
+pthread_cond_t wait = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /*
  * Permet de créer un socket entre l’application et MIC-TCP
@@ -18,7 +22,7 @@ int mic_tcp_socket(start_mode sm)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
     int result = initialize_components(sm);
-    set_loss_rate(1);
+    set_loss_rate(20);
     if(sm == CLIENT)
     {
         sock_source.fd = result;
@@ -57,7 +61,12 @@ int mic_tcp_bind(int socket, mic_tcp_sock_addr addr)
 int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-    sock_puit.state = ESTABLISHED;
+    sock_puit.state = LISTEN; 
+    pthread_mutex_lock(&mutex); 
+    while (sock_puit.state  != ESTABLISHED )
+        pthread_cond_wait(&wait,&mutex);
+    printf("[MIC-TCP]:  Connexion Etablie \n");
+    pthread_mutex_unlock(&mutex); 
     return(0);
 }
 
@@ -68,8 +77,37 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
 int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
+    mic_tcp_pdu Syn;
+    mic_tcp_pdu SynAckC; 
+    SynAckC.payload.size = 0; 
+    mic_tcp_pdu AckC;
+    Syn.header.syn = 1;
+    Syn.payload.size = 1;
+    Syn.payload.data = "4";
+    int bon = -1;
+    int test;
+    while(bon){
+        IP_send(Syn,addr);
+        sock_source.state = SYN_SENT; 
+        test = IP_recv(&SynAckC,&addr,50);
+        printf("test = %d \n", test); 
+        if(test != -1){
+            if(SynAckC.header.syn == 1 && SynAckC.header.ack == 1){
+                printf("SYN | ACK reçu \n"); 
+                bon = 0;
+            }
+        }
+    }   
+    AckC.header.ack = 1;
+    AckC.header.syn = 0;
+    AckC.header.fin = 0;
+    AckC.header.seq_num = 0; 
+    AckC.header.ack_num = 0; 
+  
+    AckC.payload.data = NULL;
+    AckC.payload.size = 0;
+    IP_send(AckC,addr);
     sock_source.state = ESTABLISHED;
-    addr_dest = addr;
     return(0);
 }
 
@@ -82,28 +120,37 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
     int renvoie =0;
     mic_tcp_pdu newPDU;
+    mic_tcp_pdu NPDU;
     int valide = -1;
     int test;
     if(mic_sock != sock_source.fd){
         return(-1);
     }
     else{
-        pdu.header.seq_num = PE;
-        pdu.payload.data = mesg;
-        pdu.payload.size = mesg_size;
+        NPDU.header.seq_num = PE;
+        NPDU.header.ack = 0;
+        NPDU.header.syn = 0;
+        NPDU.header.ack_num = 0;
+        NPDU.payload.data = mesg;
+        NPDU.payload.size = mesg_size;
         PE=(PE+1)%2;
         while(valide){
-            if((renvoie==1) && (msg_passe>=48)){
-                printf("skip\n");
+            //printf("Coucou\n");
+            if((renvoie==1) && (msg_passe>=msg_conseq)){
                 PE=(PE+1)%2;
                 msg_passe=0;
                 return(0);
             }
-            IP_send(pdu,addr_dest);
-            test = IP_recv(&newPDU,&addr_dest,100);
+            IP_send(NPDU,addr_dest);
+            test = IP_recv(&newPDU,&addr_dest,500);
             if(test != -1)
             {
-                if(newPDU.header.ack_num == PE){
+                if((newPDU.header.ack == 1) && (newPDU.header.syn == 1)){
+                    NPDU.header.ack = 1;
+                    printf("Hey\n");
+                    IP_send(NPDU,addr_dest);
+                }
+                else if(newPDU.header.ack_num == PE){
                     valide = 0;
                 }
             }
@@ -120,12 +167,13 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
  * Retourne le nombre d’octets lu ou bien -1 en cas d’erreur
  * NB : cette fonction fait appel à la fonction app_buffer_get()
  */
-int mic_tcp_recv (int socket, char* mesg, int max_mesg_size) //source
+int mic_tcp_recv (int socket, char* mesg, int max_mesg_size) 
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    pdu.payload.data = mesg;
-    pdu.payload.size = max_mesg_size;
-    return(app_buffer_get(pdu.payload));
+    mic_tcp_pdu NPDU;
+    NPDU.payload.data = mesg;
+    NPDU.payload.size = max_mesg_size;
+    return(app_buffer_get(NPDU.payload));
 }
 
 /*
@@ -148,14 +196,41 @@ int mic_tcp_close (int socket)
 void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr) //puit
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+    mic_tcp_pdu SynAck;
     mic_tcp_pdu ack;
-    if(pdu.header.seq_num == PA){
-        PA=(PA+1)%2;
-        app_buffer_put(pdu.payload);
+    int perte;
+    float taux_min;
+    if(pdu.header.syn == 1 && sock_puit.state == LISTEN){
+        pthread_mutex_lock(&mutex);
+        sock_puit.state = SYN_RECEIVED;
+        pthread_mutex_unlock(&mutex);
+        SynAck.header.syn = 1;
+        SynAck.header.ack = 1;
+        SynAck.payload.data = NULL;
+        SynAck.payload.size = 0;
+        printf("Envoi du SYN | ACK \n");
+        IP_send(SynAck,addr);
+        perte = atoi(pdu.payload.data);
+        taux_min = (float)perte/(perte+1);
+        printf("Le taux de message passé est : %f\n",taux_min*100.0);
+
+    } else if (pdu.header.ack == 1 &&  sock_puit.state == SYN_RECEIVED) {
+        pthread_mutex_lock(&mutex); 
+        printf("ACK reçu, sock state passe à ESTABLISHED \n"); 
+        sock_puit.state = ESTABLISHED; 
+        pthread_cond_broadcast(&wait); 
+        pthread_mutex_unlock(&mutex); 
+    
     }
     else{
-        printf("Pas bon \n");
+        if(pdu.header.seq_num == PA){
+            PA=(PA+1)%2;
+            app_buffer_put(pdu.payload);
+        }
+        ack.header.ack_num = PA;
+        ack.header.ack = 0;
+        ack.header.syn = 0;
+        IP_send(ack,addr);
     }
-    ack.header.ack_num = PA;
-    IP_send(ack,addr);
 }
+//gharbi.ghada@gmail.com
